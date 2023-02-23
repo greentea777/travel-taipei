df637bf92e604a798f74a1ec347c5f845b0336de
Willy <willy@Willys-MacBook-Air.local>
2023-02-22 21:01:27 -0800

    refactoring the travel data from chid to parent component(App)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { useState, useEffect } from "react";
 import ItemList from "./components/ItemList";
+import SearchItem from "./components/SearchItem";
+import db from "./database/db.json";
 
 function App() {
+  const [travelData, setTravelData] = useState(db);
+  const [search, setSearch] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const dataList = travelData.data;
   return (
     <div className="App">
-      <ItemList />
+      <SearchItem search={search} setSearch={setSearch} />
+      <ItemList travelData={dataList} />
     </div>
   );
 }
--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,15 +1,10 @@
 import React from "react";
-import { useState } from "react";
-import db from "../database/db.json";
 import Item from "./Item";
 
-const ItemList = () => {
-  const [travelData, setTravelData] = useState(db);
-
-  const dataList = travelData.data;
+const ItemList = ({ travelData }) => {
   return (
     <div className="travelItem-container">
-      {dataList.map((item) => (
+      {travelData.map((item) => (
         <Item key={item.id} item={item} />
       ))}
     </div>
