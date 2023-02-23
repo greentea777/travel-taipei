fa5c786e313745fa0dc1131d8e82c274b836bf2c
Willy <willy@Willys-MacBook-Air.local>
2023-02-23 03:26:47 -0800

    add header and menu button and nav/menu button animation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Header = () => {
+  const [isButtonOn, setIsbuttonOn] = useState(false);
+
+  const hadleMenu = () => {
+    setIsbuttonOn(!isButtonOn);
+  };
+
   return (
     <header className="header">
-      <span className="logo">Logo</span>
-      <nav>
+      <div className="header-title">
+        <span className="logo">Logo</span>
+
+        <button
+          onClick={hadleMenu}
+          className={`nav-button ${isButtonOn ? "expended" : ""}`}
+        >
+          <div className="nav-icon"></div>
+        </button>
+      </div>
+
+      <nav className={isButtonOn ? "expended" : ""}>
         <ul>
-          <li>Favourite</li>
-          <li>Sign in</li>
+          <li>
+            <a href="">Favourite</a>
+          </li>
+          <li>
+            <a href="">Sign in</a>
+          </li>
         </ul>
       </nav>
     </header>
